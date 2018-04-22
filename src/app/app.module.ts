aafd15ee4f9a1a9b3860c90772bb7282fb6871cc
Your Name <AnnaTien>
2018-04-22 13:49:54 +0700

    Change project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Routing } from "./app.routing";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from "@angular/http";
-import { HomePageComponent } from './page/HomePage/HomePage.component'; 
+import { HomePageComponent } from './page/HomePage/HomePage.component';
 import { HttpClient } from "@angular/common/http";
 import { HttpClientModule } from "@angular/common/http";
 import { MDBBootstrapModule } from './typescripts/free/index';
@@ -13,19 +13,16 @@ import { MDBBootstrapModulePro } from './typescripts/pro/index';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/Header/Header.component';
 import { FooterComponent } from './modules/Footer/Footer.component';
-import { TeammemberComponent } from './Page/Teammember/Teammember.component';
-import { RegistrationComponent } from './Page/Registration/Registration.component';
+import { HomeworkComponent } from './Page/Homework/Homework.component';
 
 @NgModule({
     declarations: [
-        AppComponent,       
+        AppComponent,
         HeaderComponent,
-        FooterComponent,       
-        HomePageComponent,
-        TeammemberComponent,
-        RegistrationComponent
-       
-         ],
+        FooterComponent, 
+        HomeworkComponent
+
+    ],
     imports: [
         BrowserModule,
         FormsModule,
@@ -34,17 +31,17 @@ import { RegistrationComponent } from './Page/Registration/Registration.componen
         Routing,
         NgbModule.forRoot(),
         MDBBootstrapModule.forRoot(),
-        MDBBootstrapModulePro.forRoot(),       
+        MDBBootstrapModulePro.forRoot(),
         HttpModule,
         HttpClientModule,
-        
+
     ],
-    
+
     bootstrap: [AppComponent],
-    schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],    
+    schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {
     constructor(private injector: Injector) {
-       
+
     }
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,9 @@
 ﻿import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './page/HomePage/HomePage.component';
-import { TeammemberComponent } from './Page/Teammember/Teammember.component';
-import { RegistrationComponent } from './Page/Registration/Registration.component';
+import { HomeworkComponent } from './Page/Homework/Homework.component';
 const routes: Routes = [
-    { path: '', redirectTo: 'registration', pathMatch: 'full' },
-    { path: 'homepage', component: HomePageComponent },
-    { path: 'teammember', component: TeammemberComponent }  ,
-    { path: 'registration', component: RegistrationComponent }
+    { path: '', redirectTo: 'homework', pathMatch: 'full' },
+    { path: 'homework', component: HomeworkComponent }
     ];
 
 export const Routing = RouterModule.forRoot(routes, { useHash: true });
