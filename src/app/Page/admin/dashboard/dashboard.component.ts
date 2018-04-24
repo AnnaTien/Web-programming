4aae3e3d474a6862dc420a7ddea3fd36891c527e
Trần Đình Trọng <tdtrong056@gmail.com>
2018-04-25 00:11:04 +0700

    add admin page

---
+++ src/app/Page/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+import {NgForOf, CurrencyPipe} from '@angular/common'
+import { Supplier, Products, Type } from '../../models/dataSample';
+import { Router } from '@angular/router';
+
+@Injectable()
+@Component({
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls:['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+    
+    constructor(){
+
+    }
+    ngOnInit(){
+
+    }
+}
