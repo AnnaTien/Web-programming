27b090286f1669fadccba56b6c47256c006fd932
Tien <nnltien@lacviet.com.vn>
2018-04-27 15:47:14 +0700

    Created data product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Routing } from "./app.routing";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from "@angular/http";
-import { HomePageComponent } from './Page/home-page/home-page.component';
 import { HttpClient } from "@angular/common/http";
 import { HttpClientModule } from "@angular/common/http";
 import { MDBBootstrapModule } from './typescripts/free/index';
@@ -16,31 +15,23 @@ import { FooterComponent } from './modules/Footer/Footer.component';
 import { HomeworkComponent } from './Page/Homework/Homework.component';
 import { RegistrationComponent } from './Page/Registration/Registration.component';
 import { TeammemberComponent } from './Page/Teammember/Teammember.component';
-import { PhoneComponent } from './Page/phone/phone.component';
-import { PadComponent } from './Page/pad/pad.component';
-import { ListProductComponent } from './Page/list-product/list-product.component';
-import { AdminComponent } from './Page/admin/admin.component';
-import { DashboardComponent } from './Page/admin/dashboard/dashboard.component';
-import { ProductComponent } from './Page/admin/product/product.component';
-import { TypeProductComponent } from './Page/admin/TypeProduct/typeproduct.component';
+import { HomePageComponent } from './page/HomePage/HomePage.component';
+import { DetailphoneComponent } from './Page/Detailphone/Detailphone.component';
+import { ShowphoneComponent } from './Page/Showphone/Showphone.component';
+
 
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        FooterComponent, 
+        FooterComponent,
         HomeworkComponent,
         HomePageComponent,
         RegistrationComponent,
         TeammemberComponent,
-        PhoneComponent,
-        PadComponent,
-        ListProductComponent,
-        AdminComponent,
-        DashboardComponent,
-        ProductComponent,
-        TypeProductComponent
+        DetailphoneComponent,
+        ShowphoneComponent
     ],
     imports: [
         BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,35 +1,19 @@
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeworkComponent } from './Page/Homework/Homework.component';
 import { RegistrationComponent } from './Page/Registration/Registration.component';
 import { TeammemberComponent } from './Page/Teammember/Teammember.component';
-import { HomePageComponent } from './Page/home-page/home-page.component';
-import { PhoneComponent } from './Page/phone/phone.component';
-import { PadComponent } from './Page/pad/pad.component';
-import { ListProductComponent } from './Page/list-product/list-product.component';
-import { AdminComponent } from './Page/admin/admin.component';
-import { DashboardComponent } from './Page/admin/dashboard/dashboard.component';
-import { ProductComponent } from './Page/admin/product/product.component';
-import { TypeProductComponent } from './Page/admin/TypeProduct/typeproduct.component';
+import { HomePageComponent } from './page/HomePage/HomePage.component';
+import { DetailphoneComponent } from './Page/Detailphone/Detailphone.component';
+import { ShowphoneComponent } from './Page/Showphone/Showphone.component';
 const routes: Routes = [
     { path: '', redirectTo: 'homepage', pathMatch: 'full' },
     { path: 'homepage', component: HomePageComponent },
-    { path: 'list-product', component: ListProductComponent },
-    { path: 'phone', component: PhoneComponent },
-    { path: 'phone/:phonecode', component: PhoneComponent },
-    { path: 'pad', component: PadComponent },
-    {   path: "admin",
-        component: AdminComponent,
-        children:[
-        { path:'', redirectTo: 'dashboard', pathMatch: 'full' },
-        { path:'dashboard', component:DashboardComponent},
-        {path:'product', component:ProductComponent},
-        {path:'typeproduct', component: TypeProductComponent}
-        ] 
-    },
-    { path: 'pad/:padcode', component: PadComponent },
+    { path: 'homework', component: HomeworkComponent },
     { path: 'registration', component: RegistrationComponent },
-    { path: 'teammember', component: TeammemberComponent }
+    { path: 'teammember', component: TeammemberComponent },
+    { path: 'detailphone', component: DetailphoneComponent },
+    { path: 'showphone', component: ShowphoneComponent }
     ];
 
 export const Routing = RouterModule.forRoot(routes, { useHash: true });
