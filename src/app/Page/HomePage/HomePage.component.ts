99bc3f31183c4c4d77c14c05a56411b2ff9ccde9
Tien <nnltien@lacviet.com.vn>
2018-04-27 15:49:38 +0700

    View list product

---
+++ src/app/Page/HomePage/HomePage.component.ts
@@ -0,0 +1,21 @@
+﻿//ndtich
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from "@angular/router";
+import { AppApi } from '../../app.api';
+@Component({
+    selector: "home-page",
+    templateUrl: "HomePage.component.html",
+    providers: [AppApi]
+})
+
+export class HomePageComponent {
+    AppApi: AppApi = new AppApi();
+    _Categories = this.AppApi.Categories;
+    _newest = this.AppApi.Products;
+    _buyest = this.AppApi.Products;
+    _viewest = this.AppApi.Products;
+    phones: any = [];
+    constructor(private routing: Router) {
+    }    
+}
