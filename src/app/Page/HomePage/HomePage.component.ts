fd02e77a79616211006dae5249ee32e36ad2b36c
nnltien <nnltien@lacviet.com.vn>
2018-06-26 00:48:23 +0700

    carousel_list product

--- src/app/Page/HomePage/HomePage.component.ts
+++ src/app/Page/HomePage/HomePage.component.ts
@@ -23,7 +23,9 @@ export class HomePageComponent implements OnInit {
     @ViewChild('carousel1') carousel1: CarouselComponent;
     @ViewChild('carousel2') carousel2: CarouselComponent;
     @ViewChild('carousel3') carousel3: CarouselComponent;
-    public doctors: any = [];
+    public doctors1: any = [];
+    public doctors2: any = [];
+    public doctors3: any = [];
     ngOnInit() {
         this.carousel1.interval = 0;
         this.carousel2.interval = 0;
@@ -32,7 +34,27 @@ export class HomePageComponent implements OnInit {
         this.http.get("http://127.0.0.1:3000/api/productlatest").subscribe(data => {
             if (data) {
                 this._newest = data;
-                console.log("lcompany", this._newest);
+                console.log("_newest", this._newest);
+                let doctor: any = [];
+                if (this._newest.length > pageSize) {
+                    for (var i = 0; i < pageSize; i++) {
+                        this._newest.push(data[i]);
+                    }
+                    for (var i = 0; i < this._newest.length - pageSize; i++) {
+                        for (var j = i; j < i + pageSize; j++) {
+                            doctor.push(this._newest[j]);
+                        }
+                        this.doctors1.push(doctor);
+                        doctor = [];
+                    }
+                }
+                else {
+                    this._newest.forEach((obj) => {
+                        doctor.push(obj);
+                    });
+                    this.doctors1.push(doctor);
+                }
+                console.log("doctors1", this.doctors1);
             }
         });
         this.http.get("http://127.0.0.1:3000/api/productdbestsell").subscribe(data => {
@@ -48,7 +70,7 @@ export class HomePageComponent implements OnInit {
                         for (var j = i; j < i + pageSize; j++) {
                             doctor.push(this._buyest[j]);
                         }
-                        this.doctors.push(doctor);
+                        this.doctors2.push(doctor);
                         doctor = [];
                     }
                 }
@@ -56,15 +78,35 @@ export class HomePageComponent implements OnInit {
                     this._buyest.forEach((obj) => {
                         doctor.push(obj);
                     });
-                    this.doctors.push(doctor);
+                    this.doctors2.push(doctor);
                 }
-                console.log("doctors", this.doctors);
+                console.log("doctors2", this.doctors2);
             }
         });
         this.http.get("http://127.0.0.1:3000/api/productmostview").subscribe(data => {
             if (data) {
                 this._viewest = data;
-                console.log("lcompany", this._viewest);
+                console.log("_viewest", this._viewest);
+                let doctor: any = [];
+                if (this._viewest.length > pageSize) {
+                    for (var i = 0; i < pageSize; i++) {
+                        this._viewest.push(data[i]);
+                    }
+                    for (var i = 0; i < this._viewest.length - pageSize; i++) {
+                        for (var j = i; j < i + pageSize; j++) {
+                            doctor.push(this._viewest[j]);
+                        }
+                        this.doctors3.push(doctor);
+                        doctor = [];
+                    }
+                }
+                else {
+                    this._viewest.forEach((obj) => {
+                        doctor.push(obj);
+                    });
+                    this.doctors3.push(doctor);
+                }
+                console.log("doctors3", this.doctors3);
             }
         });
     }
