fa6cbfd809c397aa527b62deb95857a7d0154f01
nnltien <nnltien@lacviet.com.vn>
2018-06-25 22:25:06 +0700

    api_ lấy danh sách sản phẩm bán chạy nhât ,được xem nhiều nhất

--- Models/product.js
+++ Models/product.js
@@ -1,11 +1,20 @@
 var db = require('../Dbconnection');
 
 var product = {
-  getAllProduct: function (callback) {
+  getAllProduct: function (id, key, callback) {
     return db.query("Select * from product", callback);
   },
   getProductById: function (id, callback) {
     return db.query("select * from product where product_id=?", [id], callback);
+  },
+  getProductlatest: function (callback) {
+    return db.query("Select * from product order by product_created desc", callback);
+  },
+  getProductMostview: function (callback) {
+    return db.query("Select * from product order by product_view desc", callback);
+  },
+  getProductdBestSell: function (callback) {
+    return db.query("Select * from product order by product_pay desc", callback);
   }
 };
 module.exports = product;
