fc79e8a2fec9afea4ba40af38f0c114c188c4d0a
nnltien <nnltien@lacviet.com.vn>
2018-06-25 22:26:42 +0700

    view danh sách sản phẩm theo yêu cầu(xem nhiều, bán chạy, sp mới)

--- src/app/Page/HomePage/HomePage.component.ts
+++ src/app/Page/HomePage/HomePage.component.ts
@@ -13,13 +13,30 @@ import { Subscriber } from 'rxjs/Subscriber';
 export class HomePageComponent implements OnInit {
     AppApi: AppApi = new AppApi();
     _Categories = this.AppApi.Categories;
-    _newest = this.AppApi.Products;
-    _buyest = this.AppApi.Products;
-    _viewest = this.AppApi.Products;
+    _newest: any = null;
+    _buyest: any = null;
+    _viewest: any = null;
     phones: any = [];
     constructor(private routing: Router, private http: HttpClient) {
     }
     ngOnInit() {
-
+        this.http.get("http://127.0.0.1:3000/api/productlatest").subscribe(data => {
+            if (data) {
+                this._newest = data;
+                console.log("lcompany", this._newest);
+            }
+        });
+        this.http.get("http://127.0.0.1:3000/api/productdbestsell").subscribe(data => {
+            if (data) {
+                this._buyest = data;
+                console.log("lcompany", this._buyest);
+            }
+        });
+        this.http.get("http://127.0.0.1:3000/api/productmostview").subscribe(data => {
+            if (data) {
+                this._viewest = data;
+                console.log("lcompany", this._viewest);
+            }
+        });
     }
 }
