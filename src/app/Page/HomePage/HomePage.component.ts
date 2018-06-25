5ab8181fd36e0e18b0e98794b53320f45bba8909
nnltien <nnltien@lacviet.com.vn>
2018-06-25 20:24:24 +0700

    Kết nối client với server

--- src/app/Page/HomePage/HomePage.component.ts
+++ src/app/Page/HomePage/HomePage.component.ts
@@ -1,21 +1,25 @@
-﻿//ndtich
-
-import { Component, OnInit, ViewChild } from '@angular/core';
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from "@angular/router";
 import { AppApi } from '../../app.api';
+import { HttpClient } from '@angular/common/http';
+import { Subscriber } from 'rxjs/Subscriber';
 @Component({
     selector: "home-page",
     templateUrl: "HomePage.component.html",
+
     providers: [AppApi]
 })
 
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
     AppApi: AppApi = new AppApi();
     _Categories = this.AppApi.Categories;
     _newest = this.AppApi.Products;
     _buyest = this.AppApi.Products;
     _viewest = this.AppApi.Products;
     phones: any = [];
-    constructor(private routing: Router) {
-    }    
+    constructor(private routing: Router, private http: HttpClient) {
+    }
+    ngOnInit() {
+
+    }
 }
