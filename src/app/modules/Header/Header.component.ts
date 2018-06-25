094b4f70657f7114f25a1e357fc110e257e81a7e
nnltien <nnltien@lacviet.com.vn>
2018-06-25 20:33:17 +0700

    delete function social

--- src/app/modules/Header/Header.component.ts
+++ src/app/modules/Header/Header.component.ts
@@ -1,21 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AppApi } from '../../app.api'
 import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: "eportal-header",
     templateUrl: "./Header.component.html",
-    providers: [AppApi]
 })
 
 export class HeaderComponent {
-    AppApi: AppApi = new AppApi();
-    _Categories = this.AppApi.Categories;
-    _Suppliers = this.AppApi.Suppliers;
-    newest = this.AppApi.Products;
-    buyest = this.AppApi.Products;
-    viewest = this.AppApi.Products;
-
     lcatalog: any = null;
     lcompany: any = null;
     constructor(private http: HttpClient) {
@@ -38,38 +29,6 @@ export class HeaderComponent {
             }
         });
     }
-    // hcmus() {
-    //     var urlPre = 'https://www.hcmus.edu.vn';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // courses() {
-    //     var urlPre = 'https://courses.fit.hcmus.edu.vn';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // portal() {
-    //     var urlPre = 'https://portal.hcmus.edu.vn';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // w3schools() {
-    //     var urlPre = 'https://www.w3schools.com';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // telerik() {
-    //     var urlPre = 'https://www.telerik.com';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // getbootstrap() {
-    //     var urlPre = 'https://getbootstrap.com';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // angular() {
-    //     var urlPre = 'https://angular.io';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
-    // mdbootstrap() {
-    //     var urlPre = 'https://mdbootstrap.com';
-    //     var windowChild = window.open(urlPre, "windowChild");
-    // }
     facebook() {
         var urlPre = 'https://vi-vn.facebook.com';
         var windowChild = window.open(urlPre, "windowChild");
