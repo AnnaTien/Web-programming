5ce7a3932763f00e964f17190e35f613fce44448
nnltien <nnltien@lacviet.com.vn>
2018-06-26 17:01:55 +0700

    api_Lấy thông tin công ty theo id công ty

--- Models/company.js
+++ Models/company.js
@@ -4,8 +4,8 @@ var Company = {
   getAllCompany: function (callback) {
     return db.query("Select * from company", callback);
   },
-  getCompanyById: function (id, callback) {
-    return db.query("select * from company where company_id=?", [id], callback);
+  getCompanyById: function (companyid, callback) {
+    return db.query("select * from company where company_id=?", [companyid], callback);
   }
 };
 module.exports = Company;
--- routes/routescompany.js
+++ routes/routescompany.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var Company = require('../Models/company');
-router.get('/company/:id?', function (req, res, next) {
-  if (req.params.id) {
-    Company.getCompanyById(req.params.id, function (err, rows) {
-      if (err) {
-        res.json(err);
-      } else {
-        res.json(rows);
-      }
-    });
-  } else {
-    Company.getAllCompany(function (err, rows) {
-      if (err) {
-        res.json(err);
-      } else {
-        res.json(rows);
-      }
-
-    });
-  }
+router.get('/companybyid/:companyid', function (req, res, next) {
+  var companyid = req.params.companyid;
+  Company.getCompanyById(companyid, function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows[0]);
+    }
+  });
+});
+router.get('/companyall', function (req, res, next) {
+  Company.getAllCompany(function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
 });
 module.exports = router;
